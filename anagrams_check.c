57c91dcaee1391014133cdbeca5027e2994c2ce2
ThaoNguyen <edanergowo@gmail.com>
2025-12-12 22:08:38 +0700

    update code

--- anagrams_check.c
+++ anagrams_check.c
@@ -3,6 +3,40 @@
 
 char *str1, *str2;
 
-char *anagrams(char *s1, *s2)
+bool anagram(char *s1, *s2)
 {
+    int count[256] = {0};
+    int i;
+
+    for (i = 0; s1[i] && s2[i]; i++) 
+    {
+        count[(unsigned char)s1[i]]++;
+        count[(unsigned char)s2[i]]--;
+    }
+
+    if (s1[i] || s2[i])
+        return false;
+
+    for (i = 0; i < 256; i++) 
+    {
+        if (count[i] != 0)
+            return false;
+    }
+
+    return true;
+}
+
+int main()
+{
+    printf("Enter first string: ");
+    scanf("%s", str1);
+    printf("\nEnter second string: ");
+    scanf("%s", str2);
+
+    if (anagram(str1, str2))
+        printf("The strings are anagrams.\n");
+    else
+        printf("The strings are not anagrams.\n");
+
+    return 0;
 }
