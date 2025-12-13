ea588974c6a74e4032dca2486f32fcb0e583a78f
ThaoNguyen <edanergowo@gmail.com>
2025-12-13 09:53:57 +0700

    update code

--- word_count.c
+++ word_count.c
@@ -1,1 +1,36 @@
 #include <stdio.h>
+
+char *str;
+
+int word_count(char *s)
+{
+    int i;
+    int count = 0;
+
+    while (s[0] = ' ')
+    {
+      for (i = 0; i < strlen(s); i++)
+      {
+        s[i] = s[i+1];
+      }  
+    }
+
+    for (i = 0; i < strlen(s); i++)
+    {
+        if (s[i] == ' ')
+        {
+            count ++;
+        }
+    }
+
+    return count; 
+}
+
+int main()
+{
+    printf("Enter a string: ");
+    scanf("%s", str);
+    printf("\nWords counted: %d", word_count(*str));
+
+    return 0;
+}
